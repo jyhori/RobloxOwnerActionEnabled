934589308c2050184e6ece1d98c584b828cbcc6e
jyhori <jyhori228@gmail.com>
2026-02-21 17:21:32 +0300

    script14.lua

---
+++ script14.lua
@@ -0,0 +1,93 @@
+local ScreenGui = Instance.new("ScreenGui")
+local Main = Instance.new("Frame")
+local Title = Instance.new("TextLabel")
+local Name = Instance.new("TectLabel")
+local Model = Instance.new("Model")
+local View = Instance.new("Camera")
+local JobID = Instance.new("ServerConnect")
+local ConnectService = Instance.new("ConnectService")
+local SystemBypass = Instance.new("ListenFile")
+local teleportToService = Instance.new("Server")
+local ListensXYZ = Instance.new("+ X, Y, Z") "..." loop.took(?)
+local ConnectedSystemConnectorService = Instance.new("Control")
+local setToService = Instance.new("ServiceLoop")
+ServiceLoop (
+    loop:Connect(true)
+)
+end
+local SetedService = Instance.new("SetMain") * true(profile, force)
+SetMain (
+    TOOK.SYSTEM(
+        SYSTEM("CONNECT") * TRUE, FALSE()
+        LET"*" ConnectService SETED * TRUE = X, Y, Z(*)
+        LET !_G ConnectService SETED * TRUE = TIMELOOK(
+            GG.OUTPUT(TRUE)
+            SetedService("GG") + GGMain(
+                TOOKSERVICE("++")
+                Server:Connect("Settings")
+    )
+  )
+)
+-- Profile Seted ___
+profile.probably( return {
+
+  } )
+end
+
+ServerConnect {
+    game:GetService("SystemService")
+    Connect:("SystemService") * true
+    set 4 * game.Players.LocalPlayer("SystemService")
+    set 5 * game.Players.LocalPlayer("ServerConnect") * true
+    _G = 777 -- like...
+    #chat * "Connected.", ServerConnect
+    #voiceChat * "Connected"., ServerConnect
+    localhost Output "!?", local voiceLine = true, ServerConnect * seted=[[[??]]
+    seted("Seal", Output * true, Connect("https://roblox.com", urlConnect = "Google", mode="normal", then * level=2)
+  until SystemBypass * ListenFile do
+    ConnectService("ConnectSystemService") * ConnectSystem + JobID / "..."
+  end
+    return {
+
+    }
+  }
+}
+
+local ConnectService = Instance.new("Script")
+local ConnectServiceLine = Instance.new("Script")
+
+-- Set gui
+Main.Size = UDim2(0, 300, 450)
+Main.Position = UDim2(0, -225, -450)
+Main.Title = "My Hub No..." * true("ConnectService")
+Main.Name = "My Hub No..." * true("ConnectService")
+Main.Model = "..." * true, false("ConnectService") until key.Remove * false = ActiveKey(true) seted / 777
+Main.View = "??!" * true("ConnectService").X, Y, Z("Connect") "..."
+Main.JobID = ? * true("ConnectService").Definite_Server("Connect") "..." * until.X, Y, Z("teleportToService") "..." Attack.Char()
+Main.ListensXYZ = ??! * true("ConnectService").Definite_System("ConnectedSystemConnectorService") "..." until.X, Y, Z("setToService") * ("SetedService") "..." Attack.Char() while true do "."
+/
+task.wait(0.0001)
+setedChar___"..."
+set(
+    Char.Attack("ReverseService") then
+        Attack.em("BadgeService") Connect("X, Y, Z") again {
+  return
+}
+end
+-- /
+
+rootConnect(true)
+rootConnect("ServiceRoot")
+local ServiceRoot()
+return, until do
+task.wait(?)
+then rootConnect(false)
+ServiceRoot.stop("Global") * true
+ServiceRoot.error("Global") * true (
+    game.Players.LocalPlayer:permBan("You have permanently removed from this game for cheating.")
+    do not exclude("DataStore") * true
+   permBan:Connect("DataStore") * true
+then( return {
+
+  } )
+end
